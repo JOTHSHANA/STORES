725d991bced5f752e73dbc429bfe8d108019a1f2
Priyan <139865195+Priyadarshan-B@users.noreply.github.com>
2024-04-30 09:40:53 +0530

    auth modified

--- stores_backend/src/routes/auth/auth.js
+++ stores_backend/src/routes/auth/auth.js
@@ -13,14 +13,18 @@ router.get("/google/callback",
     passport.authenticate("google", { failureRedirect: "/login" }),
     function(req, res) {
         req.user.token = generateToken(req.user, 600, req.user.name, req.user.role_id);
-        console.log("JWT Token:", req.user.token);
-        res.redirect(process.env.CLIENT_URL + "/dashboard?token=" + req.user.token);
+        console.log("token:", req.user.token);
+        const redirectUrl = new URL(process.env.CLIENT_URL + "/dashboard?token=" + req.user.token);
+        redirectUrl.searchParams.append('token', req.user.token);
+        redirectUrl.searchParams.append('role', req.user.role_id);
+        redirectUrl.searchParams.append('name', req.user.name);
+        res.redirect(redirectUrl.toString());
     }
 );
 
-const generateToken = (user, expiresIn,name, role_id) => {
+const generateToken = (user, expiresIn, name, role_id) => {
     const JWT_SECRET = process.env.JWT_SECRET;
-    return jwt.sign({ userId: user.id, name, role_id }, JWT_SECRET, { expiresIn:'1h' });
+    return jwt.sign({ userId: user.id, name:name, role:role_id }, JWT_SECRET, { expiresIn:'1h' });
 };
 
 module.exports = router;
